fda412072e78edf397ee3e08d34f7b1810b2c892
Milad Vafaeifard <milad.vafaeifard@gmail.com>
2026-05-23 16:44:14 +0200

    feat: update Next.js configuration to include dynamic basePath and improve deployment workflow

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
 
+const githubRepository = process.env.GITHUB_REPOSITORY;
+const repositoryName = githubRepository?.split('/')[1];
+const isUserOrOrgPagesSite = repositoryName?.endsWith('.github.io');
+const basePath = repositoryName && !isUserOrOrgPagesSite ? `/${repositoryName}` : '';
+
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'dist',
+  basePath,
+  images: {
+    unoptimized: true,
+  },
   async redirects() {
     return [
       { source: '/about', destination: '/#about', permanent: false },
